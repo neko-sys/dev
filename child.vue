b95034c6c54172f0be5e28f34491a1f850b75b8c
neko-sys <77562588+neko-sys@users.noreply.github.com>
2025-03-05 15:08:14 +0800

    1

---
+++ child.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		displayMsg : {{ displayMsg }}
+		msg : {{msg}}
+	</view>
+</template>
+
+<script setup>
+import { defineProps, onMounted, ref, toRef } from 'vue';
+
+// 正确定义 props 和默认值
+const props = defineProps({
+	msg: {
+		type: String,
+		default: '子组件参数'
+	}
+});
+
+// 使用 ref 创建一个本地响应式变量，而不是直接修改 prop
+const displayMsg = toRef(props.msg);
+
+onMounted(() => {
+	displayMsg.value = '114514'; // 修改本地变量而不是 prop
+});
+</script>
+
+<style></style>
---
+++ father.vue
@@ -0,0 +1,15 @@
+<template>
+	<view>
+		<childVue :msg="msg"></childVue>
+		<childVue></childVue>
+		{{msg}}
+	</view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import childVue from '@/components/child.vue';
+const msg = ref('父组件参数');
+</script>
+
+<style></style>
